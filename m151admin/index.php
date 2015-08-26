3b8cf1b003dde374afd45aa1e86a3722d76d771b
JeffMuraro7 <jeff.mrr@eduge.ch>
2015-08-26 15:00:00 +0200

    Formulaire sans tableau.

--- m151admin/index.php
+++ m151admin/index.php
@@ -17,72 +17,16 @@
 </head>
     <body>
         <div id="center">
-            <table>
-                <tr>
-                    <td>
-                        <label>Nom : </label>
-                    </td>
-                    <td>
-                        <input type="text" name="nom" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label>Prénom : </label>
-                    </td>
-                    <td>
-                        <input type="text" name="prenom" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label>Date de naissance : </label>
-                    </td>
-                    <td>
-                        <input type="date" name="birthday" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label>Description : </label>
-                    </td>
-                    <td>
-                        <textarea cols="40" name="description"></textarea>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label>Email : </label>
-                    </td>
-                    <td>
-                        <input type="email" name="mail"/>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label>Pseudo : </label>
-                    </td>
-                    <td>
-                        <input type="text" name="pseudo" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label>Mot de passe : </label>
-                    </td>
-                    <td>
-                        <input type="password" name="password" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <input type="submit">Envoyer</input>
-                    </td>
-                    <td>
-                        <input type="button">Annuler</input>
-                    </td>
-                </tr>
-            </table>
+            <div id="title">Formulaire</div>
+            <div id="formulaire">
+                <label for="nom">Nom :</label><input type="text" id="nom" /></br></br>
+                <label for="prenom">Prénom :</label><input type="text" id="prenom" /></br></br>
+                <label for="birthday">Date de naissance :</label><input type="date" id="birthday" /></br></br>
+                <label for="description">Description :</label><textarea id="description"></textarea></br></br>
+                <label for="email">Email :</label><input type="email" id="email" /></br></br>
+                <label for="pseudo">Pseudo :</label><input type="text" id="pseudo" /></br></br>
+                <label for="password">Mot de passe :</label><input type="text" id="password" /></br></br>
+            </div>
         </div>
     </body>
 </html>
