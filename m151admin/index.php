27ddbb19c474630b56eba9a317578427558619a7
JeffMuraro7 <jeff.mrr@eduge.ch>
2015-09-02 13:14:44 +0200

    Correction du formulaire. Avec un fieldset et une legend sur le formulaire.

--- m151admin/index.php
+++ m151admin/index.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+
 <?php
     /**
      * Created by PhpStorm.
@@ -8,12 +10,8 @@
 
     require_once('function.php');
     require_once('mysqLinc.php');
-
-
-
 ?>
 
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -22,19 +20,33 @@
 </head>
     <body>
         <div id="center">
-            <div id="title">Formulaire</div>
-            <div id="formulaireStyle">
-                <form id="formulaire" method="post" action="">
-                    <label for="nom">Nom :</label><input type="text" id="nom" /></br></br>
-                    <label for="prenom">Prénom :</label><input type="text" id="prenom" /></br></br>
-                    <label for="birthday">Date de naissance :</label><input type="date" id="birthday" /></br></br>
-                    <label for="description">Description :</label><textarea id="description"></textarea></br></br>
-                    <label for="email">Email :</label><input type="email" id="email" /></br></br>
-                    <label for="pseudo">Pseudo :</label><input type="text" id="pseudo" /></br></br>
-                    <label for="password">Mot de passe :</label><input type="text" id="password" /></br></br>
+            <form id="formulaire" method="post" action="">
+                <fieldset>
+                    <legend>Formulaire</legend>
+                    <label for="nom">Nom :</label>
+                    <input type="text" id="nom" /></br></br>
+
+                    <label for="prenom">Prénom :</label>
+                    <input type="text" id="prenom" /></br></br>
+
+                    <label for="birthday">Date de naissance :</label>
+                    <input type="date" id="birthday" /></br></br>
+
+                    <label for="description">Description :</label>
+                    <textarea id="description"></textarea></br></br>
+
+                    <label for="email">Email :</label>
+                    <input type="email" id="email" /></br></br>
+
+                    <label for="pseudo">Pseudo :</label>
+                    <input type="text" id="pseudo" /></br></br>
+
+                    <label for="password">Mot de passe :</label>
+                    <input type="text" id="password" /></br></br>
+
                     <input type="submit" name="valider" value="Envoyer"/> <input type="reset" name="annuler" value=" Annuler "/>
-                </form>
-            </div>
+                </fieldset>
+            </form>
         </div>
     </body>
 </html>
